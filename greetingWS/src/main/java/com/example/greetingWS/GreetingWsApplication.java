d80ad5f3cca2337b58ec5ccd06fccc010d02c3d3
aini <ainihyt.anuar@gmail.com>
2021-08-03 15:55:44 +0800

    First Commit

---
+++ greetingWS/src/main/java/com/example/greetingWS/GreetingWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.greetingWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingWsApplication.class, args);
+	}
+
+}
---
+++ greetingWS/src/test/java/com/example/greetingWS/GreetingWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetingWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
